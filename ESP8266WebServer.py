9e71c7b2073f9b15a0e89cd5419792222713bf00
codemee <meebox@gmail.com>
2018-03-13 14:14:14 +0800

    Update english comment

--- ESP8266WebServer.py
+++ ESP8266WebServer.py
@@ -1,87 +1,87 @@
-# 簡易的網頁伺服器
-# 只處理 GET
+# A simple HTTP server that only accept GET request
+# It adopt the programming style of ESP8266WebServer 
+# library in ESP8266 Arduino Core
 
 import network
 import machine
 import socket
 import uselect
 import os
-import time
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# 查詢是否有新的用戶端連線用
+# Use for checking a new client connection
 poller = uselect.poll()
-# 登記個別路徑的處理函式
+# Dict for registed handlers of all paths
 handlers = {}
-# 網頁文件的資料夾路徑
+# The path to the web documents on MicroPython filesystem
 docPath = "/"
 
-# 啟動伺服器
+# Function to start http server
 def begin(port):
   global server
   server.bind(('0.0.0.0', port))
   server.listen(1)
-  # 登記要詢問 server 是否有收到新的連線
+  # Register for checking new client connection
   poller.register(server, uselect.POLLIN)
 
-# 檢查是否有新的連線, 並加以處理新的請求
+# Check for new client connection and process the request
 def handleClient():
   global server
-  # 迷之聲, 這裡使用 0ms 當 timout 會有莫名其妙當掉的問題
-  # 會出現 Fatal exception 28(LoadProhibitedCause) 後 reset
+  # Note:don't call poll() with 0, that would randomly cause
+  # reset with "Fatal exception 28(LoadProhibitedCause)" message
   res = poller.poll(1)
-  if res:  # 表示 server 有新的連線進來
+  if res:  # There's a new client connection
     (socket, sockaddr) = server.accept()
     handle(socket)
     socket.close()
 
-# 回覆錯誤訊息給用戶端
+# Respong error meesage to client
 def err(socket, code, message):
   socket.write("HTTP/1.1 " + code + " " + message + "\r\n\r\n")
   socket.write("<h1>" + message + "</h1>")
 
-# 傳回正確的回應給用戶端  
+# Response succesful message to client
 def ok(socket, code, msg):
   socket.write("HTTP/1.1 " + code + " OK\r\n\r\n")
   socket.write(msg)
 
-# 處理新的 GET 請求
+# Processing new GET request
 def handle(socket):
   global docPath, handlers
   currLine = str(socket.readline(), 'utf-8')
   request = currLine.split(" ")
-  if len(request) != 3: # 若表頭第一行格式不對就不處理
+  if len(request) != 3: # Discarded if it's a bad header
     return
   (method, url, version) = request
-  if "?" in url: # 如果帶有參數
+  if "?" in url: # Check if there's query string?
     (path, query) = url.split("?", 2)
   else:
     (path, query) = (url, "")
   args = {}
-  if query: # 解析參數內容
+  if query: # Parsing the querying string
     argPairs = query.split("&")
     for argPair in argPairs:
       arg = argPair.split("=")
       args[arg[0]] = arg[1]
-  while True: # 一直讀取到表頭後的空行
+  while True: # Read until blank line after header
     header = socket.readline()
     if header == b"":
       return
     if header == b"\r\n":
       break
 
-  # 檢查 HTTP 版本
+  # Check for supported HTTP version
   if version != "HTTP/1.0\r\n" and version != "HTTP/1.1\r\n":
     err(socket, "505", "Version Not Supported")
-  elif method == "GET": # 本程式庫只處理 GET
-    if path.find(docPath) == 0: # 若是取用文件檔
+  elif method == "GET": # Only accept GET request
+    if path.find(docPath) == 0: # Check for path to any document
       try:
-        os.stat(path) # 檢查檔案是否存在？
-        # 送出回應表頭給用戶端
+        os.stat(path) # Check for file existence
+        # Response header first
         socket.write("HTTP/1.1 200 OK\r\n\r\n")
-        # 送出檔案內容給用戶端
+        # Response the file content
         f = open(path, "rb")
         while True:
           data = f.read(64)
@@ -89,21 +89,21 @@ def handle(socket):
             break
           socket.write(data)
         return
-      except: # 找不到名稱相符的檔案
+      except: # Can't find the file specified in path
         err(socket, "404", "Not Found")
-    elif path in handlers: # 是否為已登記處理函式的路徑
+    elif path in handlers: # Check for registered path
       handlers[path](socket, args)
     else:
       err(socket, "400", "Bad Request")
   else:
     err(socket, "501", "Not Implemented")
 
-# 登記處理指定路徑的函式
+# Register handler for processing request fo specified path
 def onPath(path, handler):
   global handlers
   handlers[path] = handler
 
-# 設定文件的資料夾位置
+# Set the path to documents' directory
 def setDocPath(path):
   global docPath
   docPath = path
